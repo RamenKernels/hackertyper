309b29edef16e3ea2159eb1b5b2354e2b60683b8
Andy Glover <andrew@hillsideghost.com>
2024-07-29 11:20:09 -0700

    formatted

--- terminal.py
+++ terminal.py
@@ -5,34 +5,38 @@ from datetime import datetime
 from colorama import Fore
 
 with open("terminal.txt", "r") as f:
-  terminal = f.read()
+    terminal = f.read()
+
 
 def get_terminal_blocks():
-  blocks = terminal.split("\n")
-  return blocks
+    blocks = terminal.split("\n")
+    return blocks
+
 
 def run_terminal():
-  line_num = 0
-  start_time = time.time()
+    line_num = 0
+    start_time = time.time()
 
-  while True:
-    try:
-      line = random.choice(get_terminal_blocks())
-      dt_string = datetime.now().strftime("%m/%d/%y %H:%M:%S")
-      print(Fore.GREEN + f"{dt_string}{line}")
-    except:
-      pass
-    end_time = time.time()
-    if end_time - start_time >= 10:
-      break
+    while True:
+        try:
+            line = random.choice(get_terminal_blocks())
+            dt_string = datetime.now().strftime("%m/%d/%y %H:%M:%S")
+            print(Fore.GREEN + f"{dt_string}{line}")
+        except:
+            pass
+        end_time = time.time()
+        if end_time - start_time >= 10:
+            break
 
-    time.sleep(random.uniform(0.01, 0.1))
+        time.sleep(random.uniform(0.01, 0.1))
+
 
 def main():
-  input("\nPress enter to run")
-  time.sleep(1)
-  run_terminal()
-  print("\nActivation Complete!")
+    input("\nPress enter to run")
+    time.sleep(1)
+    run_terminal()
+    print("\nActivation Complete!")
+
 
 if __name__ == "__main__":
     main()
--- writecode.py
+++ writecode.py
@@ -7,23 +7,21 @@ import terminal
 with open("code.txt", "r") as f:
     code = f.read()
 
+
 def get_speed() -> int:
     while True:
-            usr_input = input("Choose a typing speed >> ")
-            try:
-                if int(usr_input) < 1:
-                    print("Value must be a positive integer!")
-                else:
-                    return int(usr_input)
-            except:
+        usr_input = input("Choose a typing speed >> ")
+        try:
+            if int(usr_input) < 1:
                 print("Value must be a positive integer!")
+            else:
+                return int(usr_input)
+        except:
+            print("Value must be a positive integer!")
+
 
 def initialize() -> None:
     print(Fore.GREEN)
-
-    
-        
-
     time.sleep(0.5)
     print("Initializing.")
     time.sleep(0.5)
@@ -32,6 +30,7 @@ def initialize() -> None:
     print("Initializing...")
     time.sleep(0.5)
     print("Initialization Complete!\n")
+
 
 def writecode(type_speed) -> None:
     i = 0
@@ -49,13 +48,14 @@ def writecode(type_speed) -> None:
         i += type_speed
         if i > 6934:
             i = 0
-    
+
 
 def main():
     initialize()
     writecode(get_speed())
 
     print(Fore.RESET, end="\n", flush=False)
+
 
 if __name__ == "__main__":
     main()
