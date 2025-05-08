4fb358366f5c58f3dfcd38299c5bb1a837509fad
Andrew Glover <andrew@hillsideghost.com>
2025-05-08 07:25:54 -0700

    added a slowdown between each character so that it is much smoother

--- main.py
+++ main.py
@@ -3,47 +3,56 @@ import curses
 import time
 from colorama import Fore
 
-speed = 5
-
 with open("code.txt", "r") as f:
     code = f.read()
 
 with open("terminal.txt", "r") as g:
     terminal_lines = g.readlines()
 
-
-def get_input(stdscr):
-    key = stdscr.getch()
-    if key == 263:
-        stdscr.clear()
-        stdscr.addstr(0, 0, "Quitting the program...", curses.color_pair(1))
-        return
-    elif key == 10:
-        stdscr.clear()
-        while True:
-            for line in terminal_lines:
-                stdscr.addstr(line, curses.color_pair(1))
-                stdscr.refresh()
-                time.sleep(random.random() / 2)
-
+def run_terminal(stdscr):
+    while True:
+        for line in terminal_lines:
+            stdscr.addstr(line, curses.color_pair(1))
+            stdscr.refresh()
+            time.sleep(random.random() / 20)
 
 def main(stdscr):
+    speed = 5
+    current_index = 0
+    char_queue = []
+    frame_count = 0
+
     stdscr.clear()
 
     curses.cbreak()
     stdscr.keypad(True)
     stdscr.scrollok(True)
+    stdscr.nodelay(True)
     curses.start_color()
 
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     while True:
-        for i in range(0, len(code)):
-            if i % speed == 0:
-                get_input(stdscr)
+        key = stdscr.getch()
+        if key == 27:
+            stdscr.clear()
+            stdscr.addstr(0, 0, "Quitting the program...", curses.color_pair(1))
+            return
+        elif key == 10:
+            stdscr.clear()
+            run_terminal()
+        elif key != -1:
+            for i in enumerate(range(speed)):
+                char_queue.append(code[current_index])
+                current_index += 1
 
-            stdscr.addstr(code[i], curses.color_pair(1))
-            stdscr.refresh()
+        if char_queue != []:
+            stdscr.addstr(char_queue[0], curses.color_pair(1))
+            char_queue.pop(0)
+            frame_count = 0
+
+        frame_count += 1
+        time.sleep(0.02)
 
 
 if __name__ == "__main__":
